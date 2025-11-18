8ec7d4bac7311305016117089f7c168b000b9038
yasmine-azzaoui <yasmine-azzaoui>
2025-11-18 20:20:40 +1000

    issue with migration

---
+++ crowdfunding/fundraisers/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('fundraisers/', views.FundraiserListView.as_view()),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+    ## int:pk means we will get a unique integer ID for each fundraiser
+    path('pledges/', views.PledgeList.as_view()),
+]
---
+++ crowdfunding/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    def __str__(self):
+        # __str__ method to return the username of the user
+        return self.username 
